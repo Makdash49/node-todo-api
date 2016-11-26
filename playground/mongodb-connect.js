04f9a47a5470eafaa114cfa9d99d0ea5bae0df92
Mark McQuillen <markdmcq@hotmail.com>
2016-11-25 20:17:37 -0800

    manually generate object id using ObjectID

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,8 +1,8 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
-var user = {name: 'andrew', age: 25};
-var {name} = user;
-console.log(name);
+var obj = new ObjectID();
+console.log(obj);
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   if (err) {
