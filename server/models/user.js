3383ff448a12aaa0d6c3bbad6b9df7add33fff29
Mark McQuillen <markdmcq@hotmail.com>
2016-11-26 18:51:42 -0800

    Add POSTS /todos route and refactor mongoose

--- server/models/user.js
+++ server/models/user.js
@@ -1,16 +1,12 @@
-var Todo = mongoose.model('Todo', {
-  text: {
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User', {
+  email: {
     type: String,
     required: true,
     minlength: 1,
     trim: true
   },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
 });
+
+module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -1,63 +1,29 @@
-var {mongoose} = require(./db/mongoose);
+var express = require('express');
+var bodyParser = require('body-parser');
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  console.log(req.body);
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-// var newTodo = new Todo({
-//   text: 'Cook dinner',
-// });
-//
-// newTodo.save().then((doc) => {
-//   console.log('Saved todo', doc);
-// }, (e) => {
-//   console.log('Unable to save todo');
-// });
 
-// var otherTodo = new Todo({
-//   text: 'Something to do'
-// });
-//
-// otherTodo.save().then((doc) => {
-//   console.log('Saved todo', JSON.stringify(doc, undefined, 2));
-// }, (e) => {
-//   console.log('Unable to save todo', e);
-// });
 
-var User = mongoose.model('User', {
-  email: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
+app.listen(3000, () => {
+  console.log('Started on port 3000');
 });
-
-var user1 = new User({
-  email: 'markdmcq@hotmail.com      '
-});
-
-user1.save().then((doc) => {
-  console.log('Saved user:', JSON.stringify(doc, undefined, 2));
-}, (e) => {
-  console.log('Unable to save User', e);
-});
-
-// new User model.  Email password.  Todos will be associated.
-// Later
-
-// Setup email property. Require it.  Trim it.  Set type to string.  Setmin
-// min length of 1.
